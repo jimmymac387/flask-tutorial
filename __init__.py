b758f366498311fe0dab49974986eee9a7f8d74c
James MacCarthy <james.maccarthy@gmail.com>
2020-08-17 00:38:17 -0700

    Add authentication

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 
 
+# this is an application factory function
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
@@ -14,18 +15,24 @@ def create_app(test_config=None):
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
-        #load the test config if passed in
+        # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    #ensure the instance folder exists
+    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
     # a simple page that says hello
-    @app.rout('/hello')
+    @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
